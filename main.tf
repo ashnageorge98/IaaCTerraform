75863f8c47ccf10239ec9be18a939eaba0b1273d
asgeorge123 <ashnageorge98@gmail.com>
2024-08-06 16:32:44 -0500

    key added

--- main.tf
+++ main.tf
@@ -1,5 +1,4 @@
 # main.tf
-
 provider "azurerm" {
   features {}
 }
@@ -7,5 +6,57 @@ provider "azurerm" {
 resource "azurerm_resource_group" "example" {
   name     = "ResGp1"
   location = "East US"
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = "example-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_subnet" "example" {
+  name                 = "example-subnet"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_network_interface" "example" {
+  name                = "example-nic"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.example.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "example" {
+  name                  = "example-vm"
+  resource_group_name   = azurerm_resource_group.example.name
+  location              = azurerm_resource_group.example.location
+  size                  = "Standard_B1s"
+  admin_username        = "adminuser"
+  network_interface_ids = [azurerm_network_interface.example.id]
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC4PIYjxo9jGR+6mbYtzrfMGD6/Ps1cj++F/xrAvjeVEhP1LoCNMO1xY2+kEZ11mmQqdSUqskE6qOJemMoVW7cyt7OckEwN/w6ZiJoH1n6yE+qcXul7U1CinioXXsUAIOrJGl4BLnvS4fWIiK4//LzoIsL9byT7XzcwfE94KvWK1bgOc3PazBxbcor3TqTyzO7QIhFgPznTaXdw7Dz8L3DPm1FvEU0lA57BJwFcMWA3znAJ9Pu9sdNHFf6S4GN5yutPY0s/jYMSGMi85GF/v6gWxsfLH9keCTE/uUxgUaEN9xw6mXdjSTi40j5kDro/e7RhNmlM4tJpOYD8c5UIvNOL"
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
 }
 
