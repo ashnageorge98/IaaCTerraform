cfcf428ae902c695551ec7f753dc75b3159cd2e3
asgeorge123 <56075805+ashnageorge98@users.noreply.github.com>
2024-08-07 15:14:06 -0500

    Update modules.tf

--- modules.tf
+++ modules.tf
@@ -7,3 +7,10 @@
         ├── main.tf
         ├── variables.tf
         └── outputs.tf
+--
+
+module "resource_group" {
+  source   = "./modules/resource_group"
+  name     = var.resource_group_name
+  location = var.location
+}
