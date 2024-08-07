980c92f6859aa0c13853b5841fc976deeceb647f
asgeorge123 <56075805+ashnageorge98@users.noreply.github.com>
2024-08-07 15:19:12 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,4 +1,3 @@
-
 variable "resource_group_name" {
   description = "The name of the resource group"
   type        = string
@@ -8,4 +7,16 @@ variable "location" {
   description = "The location where resources will be created"
   type        = string
   default     = "East US"
+}
+
+variable "admin_username" {
+  description = "Admin username for the VM"
+  type        = string
+  default     = "adminuser"
+}
+
+variable "public_key" {
+  description = "SSH public key for the VM"
+  type        = string
+  default     = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC4PIYjxo9jGR+6mbYtzrfMGD6/Ps1cj++F/xrAvjeVEhP1LoCNMO1xY2+kEZ11mmQqdSUqskE6qOJemMoVW7cyt7OckEwN/w6ZiJoH1n6yE+qcXul7U1CinioXXsUAIOrJGl4BLnvS4fWIiK4//LzoIsL9byT7XzcwfE94KvWK1bgOc3PazBxbcor3TqTyzO7QIhFgPznTaXdw7Dz8L3DPm1FvEU0lA57BJwFcMWA3znAJ9Pu9sdNHFf6S4GN5yutPY0s/jYMSGMi85GF/v6gWxsfLH9keCTE/uUxgUaEN9xw6mXdjSTi40j5kDro/e7RhNmlM4tJpOYD8c5UIvNOL"
 }
