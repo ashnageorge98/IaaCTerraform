1038760df59742bd9e9a0ec30872441b6ffc5f12
asgeorge123 <56075805+ashnageorge98@users.noreply.github.com>
2024-08-07 15:18:39 -0500

    Update modules.tf

--- modules.tf
+++ modules.tf
@@ -3,14 +3,27 @@
 ├── variables.tf
 ├── terraform.tfvars
 └── modules
-    └── resource_group
+    ├── resource_group
+    │   ├── main.tf
+    │   ├── variables.tf
+    │   └── outputs.tf
+    ├── virtual_network
+    │   ├── main.tf
+    │   ├── variables.tf
+    │   └── outputs.tf
+    ├── subnet
+    │   ├── main.tf
+    │   ├── variables.tf
+    │   └── outputs.tf
+    ├── network_interface
+    │   ├── main.tf
+    │   ├── variables.tf
+    │   └── outputs.tf
+    ├── virtual_machine
+    │   ├── main.tf
+    │   ├── variables.tf
+    │   └── outputs.tf
+    └── azure_ad
         ├── main.tf
         ├── variables.tf
         └── outputs.tf
---
-
-module "resource_group" {
-  source   = "./modules/resource_group"
-  name     = var.resource_group_name
-  location = var.location
-}
