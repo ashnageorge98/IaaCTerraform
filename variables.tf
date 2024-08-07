935b1441a49099c7768d9f6420ed6bb2cd807af8
asgeorge123 <56075805+ashnageorge98@users.noreply.github.com>
2024-08-07 15:12:36 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,11 @@
 
+variable "resource_group_name" {
+  description = "The name of the resource group"
+  type        = string
+}
+
+variable "location" {
+  description = "The location where resources will be created"
+  type        = string
+  default     = "East US"
+}
